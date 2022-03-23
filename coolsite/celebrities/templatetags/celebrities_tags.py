70bf088beb6fe07db15422c07933da85380f92b2
Khasmag <khasmag06@gmail.com>
2022-03-23 15:29:06 +0300

    Add tags and refactoring views

--- coolsite/celebrities/templatetags/celebrities_tags.py
+++ coolsite/celebrities/templatetags/celebrities_tags.py
@@ -4,6 +4,19 @@ from celebrities.models import *
 register = template.Library()
 
 
-@register.simple_tag()
-def get_categories():
-    return Category.objects.all()
+@register.simple_tag(name='get_cats')
+def get_categories(filter=None):
+    if filter is None:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=filter)
+
+
+@register.inclusion_tag('celebrities/list_categories.html')
+def show_categories(sort, cat_selected=0):
+    if not sort:
+        cats = Category.objects.all()
+    else:
+        cats = Category.objects.all().order_by(sort)
+
+    return {'cats': cats, 'cat_selected': cat_selected}
--- coolsite/celebrities/views.py
+++ coolsite/celebrities/views.py
@@ -11,9 +11,7 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
 
 def index(request):
     posts = Men.objects.all()
-    cats = Category.objects.all()
     context = {'posts': posts,
-               'cats': cats,
                'menu': menu,
                'title': 'Главная страница',
                'cat_selected': 0
@@ -43,9 +41,7 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     posts = Men.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
     context = {'posts': posts,
-               'cats': cats,
                'menu': menu,
                'title': 'Главная страница',
                'cat_selected': cat_id
